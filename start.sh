44f26de6bafecf2fc4810ccf4fbce74aaee7484b
lqx <lqx@lqxdeMacBook-Pro.local>
2016-05-09 10:44:37 +0800

    add some steps in start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-
+xcodebuild -project Hello.xcodeproj -scheme Hello -sdk iphonesimulator9.3 clean
+xcodebuild -project Hello.xcodeproj -scheme Hello -sdk iphonesimulator9.3 > xcodebuild.log
+oclint-xcodebuild xcodebuild.log
+oclint-json-compilation-database -- -report-type pmd -o oclint.xml
+mkdir sonar-reports
+mv oclint.xml sonar-reports/
 sonar-runner
